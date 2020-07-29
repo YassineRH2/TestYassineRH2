5940711b5dbe73eb89ff513c57bcefb6e955bcc1
YassineRH2 <68960485+YassineRH2@users.noreply.github.com>
2020-07-29 16:08:59 +0100

    Add files via upload

---
+++ Yassine2/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Student Record',
+    'summary': '"This module will add a record to store student details"',
+    'version': '1.0',
+    'description': '"This module will add a record to store student details"',
+    'author': 'Yassine REGHIOUI',
+    'company': 'MyCompany Inc.',
+    'website': 'https://www.goole.com',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'APGL-3',
+    'data': ['Views/view.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+
+}
